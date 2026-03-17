e1c6b42666cac06aa4e8ace1b19a85474b840ed9
Nkosi Mate <nkosimate@gmail.com>
2026-03-17 19:53:46 +0000

    finish everything

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
 st.set_page_config(page_title="AI-Powered Data Analyst", layout="wide")
 st.title("🤖 AI-Powered Data Analyst Assistant")
 
@@ -24,12 +26,15 @@ if uploaded_file is not None:
     if query:
         st.info("The assistant will analyse your query and respond shortly...")
         # Load your OpenAI key from environment
-        openai_key = os.getenv("OPENAI_API_KEY")
-        if openai_key is None:
+        try:
+            openai_api_key = st.secrets["OPENAI_API_KEY"]
+        except:
+            openai_api_key = os.getenv("OPENAI_API_KEY")
+        if openai_api_key is None:
             st.error("❌ OpenAI API key not found. Set OPENAI_API_KEY as an environment variable.")
         else:
             # Initialise LLM
-            llm = OpenAI(temperature=0, openai_api_key=openai_key)
+            llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
             # Create an agent that understands Pandas data
             agent = create_pandas_dataframe_agent(llm, df, verbose=True, allow_dangerous_code=True)
             # Run the query through the agent
